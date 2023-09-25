d38b126f6147257a11623dd5bace6ef5dd4d2694
akrambj <bedjaouiakram.93@gmail.com>
2023-09-25 16:53:54 +0100

    final retouch about section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import About from "../sections/home/About";
 import Whyus from "../sections/home/Whyus";
 import Order from "../sections/home/Order";
 import Features from "../sections/home/Features";
-import logoSvg from "../assets/imgs/logoSvg.svg";
 import Transition from "../components/framerMotion/Transition";
 
 
--- src/sections/home/About.jsx
+++ src/sections/home/About.jsx
@@ -1,9 +1,49 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const About = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+        useEffect(() => {
+          const handleScroll = () => {
+            const aboutSection = document.getElementById("about");
+            if (aboutSection) {
+              const rect = aboutSection.getBoundingClientRect();
+              if (rect.top <= window.innerHeight * 0.8 && rect.bottom >= 0) {
+                setIsVisible(true);
+              } else {
+                setIsVisible(false);
+              }
+            }
+          };
+
+          window.addEventListener("scroll", handleScroll);
+
+          return () => {
+            window.removeEventListener("scroll", handleScroll);
+          };
+        }, []);
+
+        const titleVariants = {
+          hidden: { y: "-100%", opacity: 0 },
+          visible: {
+            y: 0,
+            opacity: 1,
+            transition: {
+              duration: 0.6,
+              ease: "easeIn",
+            },
+          },
+        };
   return (
-    <section className="h-[100vh] pt-16 w-full about">
-      <div className="relative w-full h-full flex items-center justify-center">
+    <section id="about" className="h-[100vh]  w-full about">
+      <motion.div
+        initial={"hidden"}
+        animate={isVisible ? "visible" : "hidden"}
+        variants={titleVariants}
+        className="relative w-full h-full flex items-center justify-center"
+      >
         <div className="text-center z-10 ">
           <p className="text-sm text-cristalux py-10 capitalize md:w-[80%] md:text-2xl lg:w-[70%] lg:text-3xl lg:leading-[40px] xs:w-[90%] xs:mx-auto xs:text-sm xs2:text-base sm:text-xl">
             Bienvenue chez notre magasin de décoration maison. Nous vous
@@ -17,7 +57,7 @@ const About = () => {
             view more
           </Link>
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 }
