21916f2fb54c596fde7b5c2d6377a36014310715
akrambj <bedjaouiakram.93@gmail.com>
2023-10-18 17:32:41 +0100

    z index brk

--- src/components/fetch/ErrorMsg.jsx
+++ src/components/fetch/ErrorMsg.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 const ErrorMsg = () => {
   return (
-    <div className="absolute top-0 right-0 left-0 bottom-0 flex items-center justify-center overlay">
-      <div className="w-[50%] h-[50%] bg-white rounded-md flex items-center justify-center flex-col gap-4">
+    <div className="absolute top-0 right-0 left-0 bottom-0 flex items-center justify-center overlay z-50">
+      <div className="w-[50%] h-[50%] bg-white rounded-md flex items-center justify-center flex-col gap-4 z-50">
         <h1 className="text-cristaluxBrown capitalize text-2xl font-bold">
           nous rencontrons une erreur, essayez d{"'"}actualiser
         </h1>
